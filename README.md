63df8089b766481fbfec6148b3e89ee8263378ee
borunx <jonatanfelizf@gmail.com>
2022-11-04 00:45:21 +0100

    falta web service

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-# Practica-1-presentaci-n-M07
-És un fitxer escrit en Markdown (veure referències) i ha de contenir les següents
-seccions:
-1.  Nom del projecte: **Diari Esportiu**
-    Nom Grup: **PARJ** 
-    Integrants: **Pau M**
-                **Jonatan F**
-                **Anthony R**
-                **Ruyou H**
-2. Descripció breu de què genera i quines dades utilitza.
-3. Execució del programa.
+# PARJ-M07-UF1-Pt2
+Nom del projecte: **Diari Esportiu**
+Nom Grup: **PARJ** 
+Integrants: **Pau M**
+            **Jonatan F**
+            **Anthony R**
+            **Ruyou H**
+
+**2.** El projecte genera diferents __.html__ a partir de diverses plantilles. Les plantilles es generen d'un fitxer __.json__, d'una taula, d'una api i d'imatges.
+
+**3.** Execució del programa.
     • Per a aquesta pràctica és obligatori fer-ho així des de l’arrel del projecte:
     • php src/generator.php
     • php -S 0.0.0.0:8080 -t public
-4. Descripció del codi font (molt breu descripció dels fitxers del directori src/.)
+
+**4.** 
+**src/lib**: Conté el fitxer utils.php, amb funcions que fer servir varies vegades al codi.
+
+**src/template**: Conté les diferents plantilles que s'utilitzen per generar els __.html__
+
+**src/generator.php**: Fitxer que executa i controla totes les accions del programa.
--- src/generator.php
+++ src/generator.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 require_once(__DIR__ . '/lib/utils.php');
 
 
-// Blog (funciona)
+// Blog
 
 function make_blog(): void {
 
@@ -17,13 +17,7 @@ function make_blog(): void {
     
 }
 
-// Galery (a medias)
-// function rewrite_path(string $local_file): string{
-//     $filename = basename($local_file);
-//     $web_link = '../db/Fotos/Logos/' . $filename;
-
-//     return $web_link;
-// }
+// Galery
 
 function make_galery(string $html_filename, string $dir_name): void {
 
@@ -38,7 +32,6 @@ function make_galery(string $html_filename, string $dir_name): void {
         $web_links[] = "../db/Fotos/$dir_name/" . $filename;
     }
 
-    //$web_links = array_map('rewrite_path', $path_image_array);
     $html_filename = "../public/$html_filename";
 
     $template_filename = 'template/galery.template.php';
@@ -48,7 +41,7 @@ function make_galery(string $html_filename, string $dir_name): void {
     file_put_contents($html_filename,$index_contents);
 }
 
-// Data Table (funciona)
+// Data Table
 
 function make_table(): void{
     
@@ -71,40 +64,10 @@ function make_table(): void{
 // Web Service
 
 function make_web_service(): void {
-
+    $data = json_decode( file_get_contents('https://www.balldontlie.io/api/v1/teams/15'), true );
+    //echo file_get_contents('https://api.football-data.org/v4/matches');
+    echo $data['city'];
 }
-
-/*Galery **************************************************
-function galery(): void{
-    $img_array = glob('../../db/Fotos/Logos/*');//recoger imagenes de carpeta
-    print_r($img_array);
-    $template_index = "src/template/galery.template.php";//la ruta template
-    $new_img_array = make_path($img_array);//array con nuevas rutas 
-    print_r($new_img_array);
-    //make_index_html($template_index, $new_img_array);
-}
-function make_path(array $img_array):array{
-    $result = [];
-    foreach($img_array as $img){
-        $path = basename($img);
-        $new_path = "/img/" . $path;
-        array_push($result, $new_path);
-    }
-    return $result;
-}
-
-function make_index_html(string $index_template_filename , array $img_array): void{
-    
-    $template_vars = ['img_array' => $img_array];//cada vez que en el template encuentre 'img_array' lo cambia a $img_array
-    $galery_html = render_template($index_template_filename, $template_vars);//devuelve plantilla del template con $template_vars cambiados
-    $index_html = "public/galery.html";//la ruta donde genera el .html, contiene 4 fotos que son albumes
-    file_put_contents($index_html, $galery_html);
-    
-}
-*/
-
-
-
 
 // Main
 
@@ -133,6 +96,6 @@ main();
 
 //Taula hecho
 
-//Web service - todo
+//Web service - falta todo
 
 //
