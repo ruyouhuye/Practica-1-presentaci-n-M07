74555e0f593a636982053f0cb79a7ba7a9dd6a1f
Jonatan Feliz <jonatanfelizf@gmail.com>
2022-11-08 11:13:54 +0100

    namespace

--- src/generator.php
+++ src/generator.php
@@ -1,17 +1,19 @@
 <?php
 declare(strict_types=1);
-require_once(__DIR__ . '/lib/utils.php');
+namespace Ssg;
 
+require_once(__DIR__ . '/lib/utils.php');
+use Utils;
 
 // Blog
 
 function make_blog(): void {
 
-    $news_array = read_json('../db/news.json');
+    $news_array = Utils\read_json('../db/news.json');
     $index_template_filename = "../src/template/blog.template.php";
     $template_vars = ['news_array' => $news_array];
 
-    $blog_contents = render_template($index_template_filename, $template_vars);
+    $blog_contents = Utils\render_template($index_template_filename, $template_vars);
     $blog_filename = "../public/blog.html";
     file_put_contents($blog_filename, $blog_contents);
     
@@ -36,7 +38,7 @@ function make_galery(string $html_filename, string $dir_name): void {
 
     $template_filename = 'template/galery.template.php';
     $template_vars     = ['images_array' => $web_links];
-    $index_contents    = render_template($template_filename,$template_vars);
+    $index_contents    = Utils\render_template($template_filename,$template_vars);
 
     file_put_contents($html_filename,$index_contents);
 }
@@ -57,7 +59,7 @@ function make_table(): void{
     $template_table= "../src/template/table.template.php";
     $filename_html = "../public/table.html";
     $template_vars = ['table' => $table];
-    $end_index = render_template($template_table,$template_vars);
+    $end_index = Utils\render_template($template_table,$template_vars);
     file_put_contents($filename_html,$end_index);
 }
 
@@ -68,7 +70,7 @@ function make_web_service(): void {
     $index_template_filename = "../src/template/service.template.php";
     $template_vars = ['webs_array' => $webs_array];
     //echo file_get_contents('https://api.football-data.org/v4/matches');
-    $web_contents = render_template($index_template_filename, $template_vars);
+    $web_contents = Utils\render_template($index_template_filename, $template_vars);
     $web_filename = "../public/web_service.html";
     file_put_contents($web_filename, $web_contents);
 }
